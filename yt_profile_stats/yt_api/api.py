efa8b2c90c0e13385c2f9b5d35bc7adebc51a8e6
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-08-22 22:33:31 +0530

    added data dump method -- dumps data to json file

--- yt_profile_stats/yt_api/api.py
+++ yt_profile_stats/yt_api/api.py
@@ -9,14 +9,15 @@ import requests
 # File base imports
 from yt_profile_stats.yt_logger.logger import yt_logger
 
-def pipeline(api_key, user_name, yt_queue):
+def pipeline(api_key, user_name):
     """ Function to call extract_user_pages and transform_data function """
     json_data = extract_user_pages(api_key=api_key, user_name=user_name)
-    transform_data(response=json_data, queue=yt_queue)
+    data = transform_data(response=json_data)
+    dump_data_to_db(data=data)
 
 def extract_user_pages(api_key, user_name):
     """ YT API to get user profile data"""
-    yt_logger.info(f'Attempting call to yt_api for username: {user_name}.')
+    yt_logger.info('Attempting API call to YOUTUBE_API for username: %s', user_name)
     response = requests.get(
         "https://www.googleapis.com/youtube/v3/channels",
         params = {
@@ -27,34 +28,59 @@ def extract_user_pages(api_key, user_name):
         timeout=500
     )
     if response.status_code == 200:
-        yt_logger.info(f'API Success, received data: {response.text}')
-        return response.text
-    yt_logger.error(f"Error while fetching data: {response.status_code}, {response.text}")
+        yt_logger.info("%s - API Success, received data: %s", response.status_code, response.text)
+        return (response.text, response.status_code)
+    else:
+        yt_logger.error("Error while fetching data: %s, %s", response.status_code, response.text)
+        return (response.text, response.status_code)
 
-def transform_data(response, queue) -> None:
+def transform_data(response) -> None:
     """ Method to clean and iterate user data """
-    resp = json.loads(response)
-    # yt_data = {
-    #     'channel_type': resp.get('items')[0]['kind'],
-    #     'channel_name': resp.get('items')[0]['snippet']['title'],
-    #     'channel_id': resp.get('items')[0]['id'],
-    #     'channel_description': resp.get('items')[0]['snippet']['description'].replace('\n\n', ' '),
-    #     'channel_created_at': resp.get('items')[0]['snippet']['publishedAt'],
-    #     'channel_logo': resp.get('items')[0]['snippet']['thumbnails']['high']['url'],
-    #     'channel_country': resp.get('items')[0]['snippet']['country'],
-    #     'channel_view_count': resp.get('items')[0]['statistics']['viewCount'],
-    #     'channel_subscriber_count': resp.get('items')[0]['statistics']['subscriberCount'],
-    #     'is_hidden_subscriber': resp.get('items')[0]['statistics']['hiddenSubscriberCount'],
-    #     'channel_video_count': resp.get('items')[0]['statistics']['videoCount'],
-    #     'privacy_channel_type': resp.get('items')[0]['status']['privacyStatus'],
-    #     'is_channel_linked': resp.get('items')[0]['status']['isLinked'],
-    #     'long_upload_allowed': resp.get('items')[0]['status']['longUploadsStatus'],
-    #     'is_kid_safe': resp.get('items')[0]['status']['madeForKids']
-    # }
+    resp = json.loads(response[0])
+    status_code = response[1]
+
+    yt_data = {
+        'channel_type': resp.get('items')[0]['kind'],
+        'channel_name': resp.get('items')[0]['snippet']['title'],
+        'channel_id': resp.get('items')[0]['id'],
+        'channel_description': resp.get('items')[0]['snippet']['description'].replace('\n\n', ' '),
+        'channel_created_at': resp.get('items')[0]['snippet']['publishedAt'],
+        'channel_logo': resp.get('items')[0]['snippet']['thumbnails']['high']['url'],
+        'channel_country': resp.get('items')[0]['snippet']['country'],
+        'channel_view_count': resp.get('items')[0]['statistics']['viewCount'],
+        'channel_subscriber_count': resp.get('items')[0]['statistics']['subscriberCount'],
+        'is_hidden_subscriber': resp.get('items')[0]['statistics']['hiddenSubscriberCount'],
+        'channel_video_count': resp.get('items')[0]['statistics']['videoCount'],
+        'privacy_channel_type': resp.get('items')[0]['status']['privacyStatus'],
+        'is_channel_linked': resp.get('items')[0]['status']['isLinked'],
+        'long_upload_allowed': resp.get('items')[0]['status']['longUploadsStatus'],
+        'is_kid_safe': resp.get('items')[0]['status']['madeForKids']
+    }
+
+    final_data = {
+        '_id': '1',
+        'created_at': '2024-03-12 00:22:11',
+        'raw_response': str(response[0]),
+        'clean_response': yt_data,
+        'response_status_code': status_code,
+        'is_success': True if status_code == 200 else False
+    }
 
     # return json object
-    queue.append(resp)
+    return final_data
 
 def dump_data_to_db(data: list):
     """ Method to dump raw data to BQ """
-    pass
+    try:
+        with open('data/data.json', 'r', encoding='utf-8') as file:
+            existing_data = json.load(file)
+    except FileNotFoundError:
+        existing_data = {}
+
+    # Merge the new data with the existing data (assuming both are lists)
+    existing_data.update(data)
+
+    # Write the updated data back to the JSON file (creates the file if it doesn't exist)
+    with open('data/data.json', 'w', encoding='utf-8') as file:
+        json.dump(existing_data, file, indent=4)
+    yt_logger.info("Data updated successfully!")
