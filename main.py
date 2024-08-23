3474575dc2a3451972a8f48b61f577715a5b87ad
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-08-23 23:07:14 +0530

    added yt_pipeline module

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ import os
 from dotenv import load_dotenv
 
 # File base imports
-from yt_profile_stats.yt_api.api import pipeline
+from yt_profile_stats.yt_api.api import YouTubeProfileWatcher
 
 if __name__ == "__main__":
     load_dotenv()
     # initialize redis client
     API_KEY = os.environ['yt_api_key']
-    CHANNEL_NAME = "@MrBeast"
-    pipeline(api_key=API_KEY, user_name=CHANNEL_NAME)
+    CHANNEL_NAME = "@BeerBiceps"
+    DATASET_NAME = 'yt_stats'
+    TABLE_NAME = 'yt_profile_stats_test'
+    yt_pipeline = YouTubeProfileWatcher(
+        api_key=API_KEY,
+        dataset_name=DATASET_NAME,
+        table_name=TABLE_NAME
+    )
+    yt_pipeline.run_pipeline(user_name=CHANNEL_NAME)
     print('Finished executing script..')
