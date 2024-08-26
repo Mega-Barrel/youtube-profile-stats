ea879c41a095ab826203197daaa72a80b21773bd
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-08-26 21:50:55 +0530

    changed channel_name

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     load_dotenv()
     # initialize redis client
     API_KEY = os.environ['yt_api_key']
-    CHANNEL_NAME = "RanveerAllahbadiaasada"
+    CHANNEL_NAME = "BeerBiceps"
     DATASET_NAME = 'yt_stats'
     TABLE_NAME = 'yt_profile_stats'
     yt_pipeline = YouTubeProfileWatcher(
