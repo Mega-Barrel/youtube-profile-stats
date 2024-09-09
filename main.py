862c7896e947e17bc1188d74068edd1090fbd89b
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-09-09 12:03:47 +0530

    minor change: Changed API KEY variable name to upper case

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from yt_profile_stats.yt_api.api import YouTubeProfileWatcher
 if __name__ == "__main__":
     load_dotenv()
     # initialize redis client
-    API_KEY = os.environ['yt_api_key']
+    API_KEY = os.environ['YT_API_KEY']
     CHANNEL_NAME = "BeerBiceps"
     DATASET_NAME = 'yt_stats'
     TABLE_NAME = 'yt_profile_stats'
