96ccd01489f5e03a3cbdfb5660a1ff00dfe16437
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-10-08 14:42:53 +0530

    refactored code

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 """ Main method to hit api """
 
 # System level imports
-import os
-from dotenv import load_dotenv
 
 # File base imports
-from yt_profile_stats.yt_api.api import YouTubeProfileWatcher
+from yt_profile_stats.pipeline.extract import extract_user_pages
+from yt_profile_stats.pipeline.transform import transform_data
+from yt_profile_stats.pipeline.load import dump_data_to_db
+
+def run_pipeline(channel_name):
+    """Orchestrates the ETL pipeline for fetching and processing YouTube profile data."""
+    # Extract
+    json_data = extract_user_pages(channel_name=channel_name)
+    # Load
+    dump_data_to_db(raw_data=transform_data(resp=json_data))
 
 if __name__ == "__main__":
-    load_dotenv()
-    # initialize redis client
-    API_KEY = os.environ['YT_API_KEY']
-    CHANNEL_NAME = "BeerBiceps"
-    yt_pipeline = YouTubeProfileWatcher(
-        api_key=API_KEY
-    )
-    yt_pipeline.run_pipeline(user_name=CHANNEL_NAME)
-    print('Finished executing script..')
+    CHANNEL_NAME = 'pinkvilla'
+    run_pipeline(channel_name=CHANNEL_NAME)
+    print('Finished Executing Script..')
