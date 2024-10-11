05a389d52675146c1224baa634c94b2490a7a262
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-10-11 15:17:55 +0530

    added pandas support to read CSV file for list of YouTube Profile

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """ Main method to hit api """
 
-# System level imports
+import pandas as pd
 
 # File base imports
 from yt_profile_stats.pipeline.extract import extract_user_pages
-from yt_profile_stats.pipeline.transform import transform_data
 from yt_profile_stats.pipeline.load import dump_data_to_db
 
 def run_pipeline(channel_name):
@@ -12,9 +11,12 @@ def run_pipeline(channel_name):
     # Extract
     json_data = extract_user_pages(channel_name=channel_name)
     # Load
-    dump_data_to_db(raw_data=transform_data(resp=json_data))
+    dump_data_to_db(resp=json_data)
 
 if __name__ == "__main__":
-    CHANNEL_NAME = 'BeerBiceps'
-    run_pipeline(channel_name=CHANNEL_NAME)
+    df = pd.read_csv('youtubers_list.csv')
+    for index, row in df.iterrows():
+        channelName = row['YouTube URL']
+
+        run_pipeline(channel_name=channelName.split("/@")[1])
     print('Finished Executing Script..')
